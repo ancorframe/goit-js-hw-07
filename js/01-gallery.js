16289b4b0ff099520640af1cddd2b9a5ecb7b1dc
ancorframe <yura.pundiak@gmail.com>
2022-08-23 22:20:15 +0300

    +

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,68 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const galleryEl = document.querySelector(".gallery");
+
+const galleryItemsRender = galleryItems
+  .map(({ preview, original, description }) => {
+    return `<div class="gallery__item">
+  <a class="gallery__link" href=${original}>
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source=${original}
+      alt=${description}
+    />
+  </a>
+</div>`;
+  })
+  .join("");
+
+// console.log(galleryItemsRender);
+galleryEl.innerHTML = galleryItemsRender;
+
+
+const linkprev = galleryEl.querySelectorAll(".gallery__link");
+linkprev.forEach((link) =>
+  link.addEventListener("click", (e) => {
+    e.preventDefault();
+  })
+);
+
+
+
+galleryEl.addEventListener("click", onImageClick);
+
+function onImageClick(event) {
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+  lightBoxOn(event.target.dataset.source);
+}
+
+function lightBoxOn(e) {
+  const instance = basicLightbox.create(`
+    <img src=${e} width="800" height="600">
+`);
+    
+  instance.show();
+
+  if (instance.visible()) {
+    window.addEventListener("keydown", closeModal);
+  }
+
+  function closeModal(e) {
+    if (e.code === "Escape") {
+        instance.close();
+        window.removeEventListener("keydown", closeModal);
+    }
+  }
+}
+
+
+
+
+
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,24 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery");
+
+const galleryItemsRender = galleryItems
+  .map(({ preview, original, description }) => {
+    return `<a class="gallery__item" href=${original}>
+  <img class="gallery__image" src=${preview} alt=${description} />
+</a>`;
+  })
+  .join("");
+
+// console.log(galleryItemsRender);
+galleryEl.innerHTML = galleryItemsRender;
+
+new SimpleLightbox(".gallery a", {
+  /* options */
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay:250,
+});
+
+
